821345e7430376d782ec31108a5d8b9678881e89
Roy Smart <roytsmart@gmail.com>
2024-08-14 20:57:13 -0600

    Added `furst_optics.FrontAperture` class. (#9)

--- furst_optics/__init__.py
+++ furst_optics/__init__.py
@@ -3,7 +3,9 @@ An idealized raytrace model of the FURST optical system.
 """
 
 from ._sources import SolarDisk
+from ._front_apertures import FrontAperture
 
 __all__ = [
     "SolarDisk",
+    "FrontAperture",
 ]
