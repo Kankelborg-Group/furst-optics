891e83345fa89294fb570e1a7dd7c6e03e98dd24
Roy Smart <roytsmart@gmail.com>
2024-08-16 14:31:48 -0600

    Added `furst_optics.feed_optics.materials.coating_design()` function.

---
+++ furst_optics/feed_optics/materials/__init__.py
@@ -0,0 +1,7 @@
+from ._materials import (
+    coating_design,
+)
+
+__all__ = [
+    "coating_design",
+]
---
+++ furst_optics/feed_optics/materials/_materials.py
@@ -0,0 +1,80 @@
+import astropy.units as u
+import optika
+
+__all__ = [
+    "coating_design",
+]
+
+
+def coating_design() -> optika.materials.MultilayerMirror:
+    """
+    The as-designed coating for the FURST feed optics.
+
+    Examples
+    --------
+
+    Plot the efficiency of the coating across the VUV range
+
+    .. jupyter-execute::
+
+        import matplotlib.pyplot as plt
+        import astropy.units as u
+        import named_arrays as na
+        import optika
+        import furst_optics
+
+        # Define an array of wavelengths with which to sample the efficiency
+        wavelength = na.geomspace(100, 600, axis="wavelength", num=101) * u.nm
+
+        # Define the incident rays from the wavelength array
+        rays = optika.rays.RayVectorArray(
+            wavelength=wavelength,
+            direction=na.Cartesian3dVectorArray(0, 0, 1),
+        )
+
+        # Initialize the FURST feed optic material
+        material = furst_optics.feed_optics.materials.coating_design()
+
+        # Compute the reflectivity of the primary mirror
+        reflectivity = material.efficiency(
+            rays=rays,
+            normal=na.Cartesian3dVectorArray(0, 0, -1),
+        )
+
+        # Plot the reflectivity vs wavelength
+        fig, ax = plt.subplots(constrained_layout=True)
+        na.plt.plot(wavelength, reflectivity, ax=ax);
+        ax.set_xlabel(f"wavelength ({wavelength.unit:latex_inline})");
+        ax.set_ylabel("reflectivity");
+    """
+    return optika.materials.MultilayerMirror(
+        layers=[
+            optika.materials.Layer(
+                chemical="MgF2",
+                thickness=25 * u.nm,
+                # interface=optika.materials.profiles.ErfInterfaceProfile(2 * u.nm),
+                kwargs_plot=dict(
+                    color="tab:blue",
+                    alpha=0.3,
+                ),
+            ),
+            optika.materials.Layer(
+                chemical="Al",
+                thickness=50 * u.nm,
+                # interface=optika.materials.profiles.ErfInterfaceProfile(2 * u.nm),
+                kwargs_plot=dict(
+                    color="tab:blue",
+                    alpha=0.5,
+                ),
+            ),
+        ],
+        substrate=optika.materials.Layer(
+            chemical="SiO2",
+            thickness=3 * u.mm,
+            # interface=optika.materials.profiles.ErfInterfaceProfile(2 * u.nm),
+            kwargs_plot=dict(
+                color="gray",
+                alpha=0.5,
+            ),
+        ),
+    )
