30c3340741d092964a53d3662478f502b1ba38bc
Roy Smart <roytsmart@gmail.com>
2024-08-15 20:17:03 -0600

    Added `furst_optics.feed_optics.FeedOptic` class.

--- furst_optics/__init__.py
+++ furst_optics/__init__.py
@@ -2,10 +2,13 @@
 An idealized raytrace model of the FURST optical system.
 """
 
+from . import abcs
 from ._sources import SolarDisk
 from ._front_apertures import FrontAperture
+from . import feed_optics
 
 __all__ = [
+    "abcs",
     "SolarDisk",
     "FrontAperture",
 ]
---
+++ furst_optics/feed_optics/_feed_optics_test.py
@@ -0,0 +1,21 @@
+import pytest
+import astropy.units as u
+import furst_optics._rowland_components_test
+
+
+@pytest.mark.parametrize(
+    argnames="a",
+    argvalues=[
+        furst_optics.feed_optics.FeedOptic(
+            radius=3 * u.mm,
+            aperture_subtent=10 * u.deg,
+            aperture_height=10 * u.mm,
+            rowland_radius=1000 * u.mm,
+            rowland_azimuth=10 * u.deg,
+        )
+    ]
+)
+class TestFeedOptics(
+    furst_optics._rowland_components_test.AbstractTestAbstractRowlandComponent
+):
+    pass
