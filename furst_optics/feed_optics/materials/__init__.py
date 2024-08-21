01ab40d88c4e9a9869e15415405dce5aeb3a3c4b
Roy Smart <roytsmart@gmail.com>
2024-08-20 21:55:51 -0600

    witness measurements

--- furst_optics/feed_optics/materials/__init__.py
+++ furst_optics/feed_optics/materials/__init__.py
@@ -1,7 +1,9 @@
 from ._materials import (
     coating_design,
+    coating_witness_measured,
 )
 
 __all__ = [
     "coating_design",
+    "coating_witness_measured",
 ]
--- furst_optics/feed_optics/materials/_materials.py
+++ furst_optics/feed_optics/materials/_materials.py
@@ -1,14 +1,19 @@
+import pathlib
+import numpy as np
 import astropy.units as u
+import named_arrays as na
 import optika
 
 __all__ = [
     "coating_design",
+    "coating_witness_measured",
 ]
 
 
 def coating_design() -> optika.materials.MultilayerMirror:
     """
-    The as-designed coating for the FURST feed optics.
+    The as-designed coating for the FURST feed optics, Acton Optics
+    broadband VUV coating #1200.
 
     Examples
     --------
@@ -41,13 +46,13 @@ def coating_design() -> optika.materials.MultilayerMirror:
         # Initialize the FURST feed optic material
         material = furst_optics.feed_optics.materials.coating_design()
 
-        # Compute the reflectivity of the primary mirror
+        # Compute the reflectivity of the feed optics
         reflectivity = material.efficiency(
             rays=rays,
             normal=na.Cartesian3dVectorArray(0, 0, -1),
         )
 
-        # Plot the reflectivity vs wavelength
+        # Plot the reflectivity of the feed optics vs wavelength
         fig, ax = plt.subplots(constrained_layout=True)
         na.plt.plot(wavelength, reflectivity, ax=ax);
         ax.set_xlabel(f"wavelength ({wavelength.unit:latex_inline})");
@@ -84,3 +89,93 @@ def coating_design() -> optika.materials.MultilayerMirror:
             ),
         ),
     )
+
+
+def coating_witness_measured() -> optika.materials.MeasuredMirror:
+    """
+    A reflectivity measurement of the witness samples to the
+    feed optics.
+
+    Examples
+    --------
+    Load the witness sample measurement and plot it as a function
+    of wavelength against the modeled reflectivity.
+
+    .. jupyter-execute::
+
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import astropy.visualization
+        import named_arrays as na
+        import optika
+        import furst_optics
+
+        # Load the coating model
+        coating_model = furst_optics.feed_optics.materials.coating_design()
+
+        # Load the model and the witness sample measurements
+        coating_measurement = furst_optics.feed_optics.materials.coating_witness_measured()
+        measurement = coating_measurement.efficiency_measured
+
+        # Isolate the wavelengths of the measurement
+        wavelength = measurement.inputs.wavelength
+
+        # Isolate the incidence angle of the measurement
+        angle = measurement.inputs.direction
+
+        # Calculate the reflectivity of the model for the same
+        # wavelengths as the measurements
+        reflectivity_model = coating_model.efficiency(
+            rays=optika.rays.RayVectorArray(
+                wavelength=wavelength,
+                direction=na.Cartesian3dVectorArray(
+                    x=np.sin(angle),
+                    y=0,
+                    z=np.cos(angle),
+                ),
+            ),
+            normal=na.Cartesian3dVectorArray(0, 0, -1),
+        )
+
+        # Plot the measurement as a function of wavelength
+        with astropy.visualization.quantity_support():
+            fig, ax = plt.subplots(constrained_layout=True)
+            na.plt.plot(
+                wavelength,
+                reflectivity_model,
+                axis="wavelength",
+                ax=ax,
+                label="model",
+            )
+            na.plt.plot(
+                measurement.inputs.wavelength,
+                measurement.outputs,
+                ax=ax,
+                label="measurement",
+            )
+            ax.set_xlabel(f"wavelength ({measurement.inputs.wavelength.unit:latex_inline})");
+            ax.set_ylabel("reflectivity");
+            ax.legend();
+    """
+    wavelength, reflectivity = np.loadtxt(
+        fname=pathlib.Path(__file__).parent / "_data/witness-2023-May-24.txt",
+        skiprows=1,
+        unpack=True,
+    )
+    wavelength = na.ScalarArray(wavelength << u.nm, axes="wavelength")
+    reflectivity = na.ScalarArray(reflectivity << u.percent, axes="wavelength")
+
+    result = optika.materials.MeasuredMirror(
+        efficiency_measured=na.FunctionArray(
+            inputs=na.SpectralDirectionalVectorArray(
+                wavelength=wavelength,
+                direction=15 * u.deg,
+            ),
+            outputs=reflectivity,
+        ),
+        substrate=optika.materials.Layer(
+            chemical="SiO2",
+        ),
+    )
+
+    return result
