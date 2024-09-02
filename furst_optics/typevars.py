b365f8537ef47b0cb10c006f7aaf8bdd5d98c9a4
Roy Smart <roytsmart@gmail.com>
2024-09-02 15:21:06 -0600

    docs

--- furst_optics/typevars.py
+++ furst_optics/typevars.py
@@ -1,3 +1,8 @@
+"""
+Type variables used by the generic types
+in this project.
+"""
+
 from typing import TypeVar
 import optika
 
@@ -9,16 +14,16 @@ __all__ = [
 
 
 #: Generic sag type.
-#: Should be :obj:`None` or an instance of
+#: Should be :obj:`None` or a subclass of
 #: :class:`optika.sags.AbstractSag`
 SagT = TypeVar("SagT", bound=None | optika.sags.AbstractSag)
 
 #: Generic material type.
-#: Should be :obj:`None` or an instance of
+#: Should be :obj:`None` or a subclass of
 #: :class:`optika.materials.AbstractMaterial`
 MaterialT = TypeVar("MaterialT", bound=None | optika.materials.AbstractMaterial)
 
 #: Generic ruling type.
-#: Should be :obj:`None` or an instance of
+#: Should be :obj:`None` or a subclass of
 #: :class:`optika.rulings.AbstractRulings`
 RulingT = TypeVar("RulingT", bound=None | optika.rulings.AbstractRulings)
